f418dd34559521c05bcfdc919462cae40a043322
Antonio Salinas <antoniosalinas2410@gmail.com>
2025-01-14 11:56:01 -0600

    impresion variables

---
+++ impresion_variables.py
@@ -0,0 +1,15 @@
+#impresion de variables
+nombre = "Kevin"
+print (nombre)
+
+numero_uno = 10
+numero_dos = 5
+
+#impresion de sumas
+print(numero_uno+numero_dos)
+
+#impresion de concatenaciones
+nombre_completo = "Kevin Armando"
+apellidos_completos= "Luciano Garcia"
+
+print(nombre_completo + " " + apellidos_completos)
