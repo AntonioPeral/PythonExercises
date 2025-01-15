7830e532650ca945f0c506cbd3647d4786a0a3e2
Antonio Salinas <antoniosalinas2410@gmail.com>
2025-01-15 11:21:00 -0600

    operaciones matematicas

---
+++ operaciones.py
@@ -0,0 +1,17 @@
+#Definicion de dos variables en python para hacer calculos matematicos
+
+#Definicion de variables
+a = 10
+b = 5
+
+#Operaciones matematicas
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b 
+
+#Impresion de resultados
+print ("La suma de a y b es: ", suma)
+print ("la resta de a y b es: ", resta)
+print ("la multipliacion de a y b ", multiplicacion)
+print ("la division de a y b es : ", division)
